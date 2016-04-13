35d1c9b520f54ad54ddde4978fbc319f2d2ca54c
taptap <alexey.e.egorov@gmail.com>
2016-04-13 09:43:36 +0300

    add rt init

---
+++ rt/src/main.rs
@@ -0,0 +1,34 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+fn main() {
+    let context = sdl2::init().unwrap();
+    let video = context.video().unwrap();
+    let window = video.window("demo window", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer().build().unwrap();
+    renderer.set_draw_color(Color::RGB(255, 128, 64));
+    renderer.clear();
+    renderer.present();
+
+    let mut pump = context.event_pump().unwrap();
+
+    'running: loop {
+        for event in pump.poll_iter() {
+            match event {
+                Event::Quit {..}
+                | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => ()
+            }
+        }
+    }
+}
