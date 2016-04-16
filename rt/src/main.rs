91ec1f3203e992defc17c69596315bb6db32dcdc
taptap <alexey.e.egorov@gmail.com>
2016-04-17 01:39:49 +0300

    implemet parts of raytracer
    Also stub sdl window with random generated noise

--- rt/src/main.rs
+++ rt/src/main.rs
@@ -1,20 +1,33 @@
 extern crate sdl2;
+extern crate nalgebra as na;
+extern crate rand;
 
-use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::TextureAccess;
+
+const HEIGHT: u32 = 800;
+const WIDTH: u32 = 600;
+
+mod raytracer;
 
 fn main() {
     let context = sdl2::init().unwrap();
     let video = context.video().unwrap();
-    let window = video.window("demo window", 800, 600)
+    let window = video.window("demo window", HEIGHT, WIDTH)
         .position_centered()
         .opengl()
         .build()
         .unwrap();
 
     let mut renderer = window.renderer().build().unwrap();
-    renderer.set_draw_color(Color::RGB(255, 128, 64));
+    let mut texture = renderer.create_texture(PixelFormatEnum::RGB888,
+                                              TextureAccess::Static,
+                                              WIDTH, HEIGHT).unwrap();
+    let mut pixels: [u8; WIDTH as usize * HEIGHT as usize * 3] =
+        [0; WIDTH as usize * HEIGHT as usize * 3];
+
     renderer.clear();
     renderer.present();
 
@@ -30,5 +43,12 @@ fn main() {
                 _ => ()
             }
         }
+        for i in 0..pixels.len() {
+            pixels[i] = rand::random::<u8>();
+        }
+        texture.update(None, &pixels, WIDTH as usize);
+        renderer.clear();
+        renderer.copy(&texture, None, None);
+        renderer.present();
     }
 }
--- rt/src/raytracer.rs
+++ rt/src/raytracer.rs
@@ -1,7 +1,58 @@
-extern crate nalgebra as na;
+use na::Vec3;
+use std::f64::consts::PI;
+use std::f64::INFINITY;
+use na::Norm;
 
-fn march () {
-    loop {
+use super::{HEIGHT, WIDTH};
+
+struct Ray {
+    dir: Vec3<f64>,
+    origin: Vec3<f64>
+}
+
+struct Sphere {
+    radius: f64,
+    origin: Vec3<f64>
+}
+
+struct Camera {
+    up: Vec3<f64>,
+    eye: Vec3<f64>,
+    look_at: Vec3<f64>,
+    fov: f64,
+    width: u32,
+    height: u32
+}
+
+fn march (cam: Camera, spheres: &Vec<Box<Sphere>>) -> Vec<Vec3<u8>> {
+    let aspect: f64 = cam.width as f64 / cam.height as f64;
+    let angle = (PI * 0.5 * cam.fov / 180.).tan();
+    let invWidth = 1. / cam.width as f64;
+    let invHeight = 1. / cam.height as f64;
+    let mut pixels: Vec<Vec3<u8>> = Vec::with_capacity((cam.height * cam.width) as usize);
+
+    for x in 0..cam.width {
+        for y in 0..cam.height {
+            let xx: f64 = (2. * ((x as f64 + 0.5) * invWidth) - 1.) * angle * aspect;
+            let yy: f64 = (1. - 2. * ((y as f64 + 0.5) * invHeight)) * angle;
+            let mut ray: Vec3<f64> = Vec3 { x: xx, y: yy, z: -1. };
+            ray.normalize_mut();
+            let (r, g, b) = trace(&cam.eye, &ray, spheres);
+            pixels[(x * cam.width + y) as usize].x = r;
+            pixels[(x * cam.width + y) as usize].y = g;
+            pixels[(x * cam.width + y) as usize].z = b;
+        }
+    }
+    pixels
+}
+
+fn trace(oriign: &Vec3<f64>, dir: &Vec3<f64>, spheres: &Vec<Box<Sphere>>)
+         -> (u8, u8, u8) {
+    let mut near: f64 = INFINITY;
+    let mut t0: f64;
+    let mut t1: f64;
+    for sphere in spheres {
 
     }
+    (6, 6, 6)
 }
