3e1b66dddfbdf9936b0a4ff33e507daa714b0306
alex-eg <alexey.e.egorov@gmail.com>
2019-01-24 17:18:30 +0300

    serialization wip

--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,4 @@
+use serde::ser::{Serialize, SerializeStruct};
 use std::fmt::Debug;
 
 #[derive(Clone, Copy)]
@@ -6,4 +7,17 @@ pub struct Vec3<T: Eq + Copy + Debug + Clone> {
     x: T,
     y: T,
     z: T,
+}
+
+impl<T> Serialize for Vec3<T> {
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = s.serialize_struct("Vec3", 3)?;
+        state.serialize_field("x", &self.x)?;
+        state.serialize_field("y", &self.y)?;
+        state.serialize_field("z", &self.z)?;
+        state.end()
+    }
 }
