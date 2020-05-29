62dfcad6d6b54487af8fe370fb80a4ac1dbe81e2
alex-eg <alexey.e.egorov@gmail.com>
2020-05-29 20:52:15 +0300

    Make scene readable from the disk

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,7 @@
-use crate::animation::Animation;
-use crate::camera::CamBuilder;
 use crate::fps_counter::FpsCounter;
 use crate::input::InputHandler;
-use crate::light::Light;
-use crate::material::Material;
-use crate::object::{new_box, new_sphere, new_square, new_triangle, Object};
 use crate::raytracer::march;
 use crate::scene::Scene;
-
-use crate::math::Vec3f;
 
 use sdl2::event::Event;
 use sdl2::pixels::PixelFormatEnum;
@@ -19,8 +12,6 @@ const CAM_HEIGHT: u32 = 480;
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
-
-use ron::ser::{to_string_pretty, PrettyConfig};
 
 pub fn run() {
     let context = sdl2::init().unwrap();
@@ -52,91 +43,8 @@ pub fn run() {
     const PIX_SIZE: usize = CAM_WIDTH as usize * CAM_HEIGHT as usize * 3;
     let mut pixels: [u8; PIX_SIZE] = [0; PIX_SIZE];
 
-    let red = Material {
-        ambient: Vec3f::new(0.1, 0.1, 0.1),
-        diffuse: Vec3f::new(1., 0., 0.),
-        specular: Vec3f::new(1., 1., 1.),
-        shininess: 30.0,
-        reflection: 0.8,
-        refraction: 0.,
-    };
-
-    let blue = Material {
-        ambient: Vec3f::new(0.1, 0.1, 0.1),
-        diffuse: Vec3f::new(0., 0.3, 1.),
-        specular: Vec3f::new(1., 1., 1.),
-        shininess: 10.0,
-        reflection: 0.4,
-        refraction: 1.025,
-    };
-
-    let green = Material {
-        ambient: Vec3f::new(0.1, 0.1, 0.1),
-        diffuse: Vec3f::new(0., 1., 0.3),
-        specular: Vec3f::new(1., 1., 1.),
-        shininess: 10.0,
-        reflection: 0.8,
-        refraction: 0.,
-    };
-
-    let camera = CamBuilder::new()
-        .eye(Vec3f::new(0., 0., 60.))
-        .center(Vec3f::new(0., 0., 59.))
-        .fov(30.)
-        .width(CAM_WIDTH)
-        .height(CAM_HEIGHT)
-        .up(Vec3f::new(0., -1., 0.))
-        .build();
-
-    let mut sphere1 = new_sphere("s1", Vec3f::new(15., 15., 15.), 5., green);
-    let mut box1 = new_box(
-        "b1",
-        Vec3f::new(10., 10., -20.),
-        Vec3f::new(10., 10., 10.),
-        red,
-    );
-    let mut sphere3 = new_sphere("s5", Vec3f::new(-15., 15., 15.), 5., blue);
-    let mut sphere4 = new_sphere("s6", Vec3f::new(-15., 15., -15.), 5., red);
-    let sphere5 = new_sphere("s3", Vec3f::new(15., -15., 15.), 5., green);
-    let sphere6 = new_sphere("s4", Vec3f::new(15., -15., -15.), 5., blue);
-    let sphere7 = new_sphere("s7", Vec3f::new(-15., -15., 15.), 5., red);
-    let sphere8 = new_sphere("s8", Vec3f::new(-15., -15., -15.), 5., green);
-    sphere1.set_animation(Animation::new(&sphere1, &[Vec3f::new(0., -10., 0.)]));
-    box1.set_animation(Animation::new(&box1, &[Vec3f::new(0., 7., 0.)]));
-    sphere3.set_animation(Animation::new(&sphere3, &[Vec3f::new(0., -8., 0.)]));
-    sphere4.set_animation(Animation::new(&sphere4, &[Vec3f::new(0., 5., 0.)]));
-
-    let triangle = new_triangle(
-        "tri1",
-        Vec3f::new(0., -10., -10.),
-        Vec3f::new(-10., 10., 0.),
-        Vec3f::new(10., 10., 0.),
-        blue,
-    );
-
-    let objects: Vec<Object> = vec![
-        sphere1,
-        box1,
-        sphere3,
-        sphere4,
-        sphere5,
-        sphere6,
-        sphere7,
-        triangle,
-        sphere8,
-        new_square("square", Vec3f::new(0., 30., 0.), 128, blue),
-    ];
-
-    let light1 = Light {
-        pos: Vec3f::new(0., -60., 0.),
-        color: Vec3f::new(1., 1., 1.),
-    };
-    let lights: Vec<Light> = vec![light1];
-
-    let mut scene: Scene = Scene{ cam: camera, objects: objects, lights: lights };
-
-    let s = to_string_pretty(&scene, PrettyConfig::default()).unwrap();
-    println!("{}", s);
+    let lines = std::fs::read_to_string("src/scene.ron").expect("Can't read scene.ron");
+    let mut scene: Scene = ron::de::from_str(&lines).unwrap();
 
     canvas.clear();
     canvas.present();
